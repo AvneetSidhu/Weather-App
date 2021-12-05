d071be605dbfdf5f874e50ff66d4cce7b0337e84
AvneetSidhu <56938074+AvneetSidhu@users.noreply.github.com>
2021-12-04 21:56:10 -0500

    Add files via upload

---
+++ weatherApp/weatherApp.js
@@ -0,0 +1,56 @@
+window.addEventListener('load', ()=>{
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let locationA = document.querySelector(".location-timezone");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let degSection = document.querySelector(".degree-section");
+    let degSpan = document.querySelector(".degree-section span");
+    let tempHumidity = document.querySelector(".temperature-humidity");
+    let dayMaxMin = document.querySelector('.temperature-maxmin');
+
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            let proxy = 'https://cors-anywhere.herokuapp.com/';
+
+            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=17e443ff35a9b00d00d9f98d295acc9f&units=metric`;
+
+            fetch(api)
+                .then(response => { 
+                    return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                const {temp} = (data.main);
+                const location = data.name;
+                const iconNum = data.weather[0].id;
+                const humid = data.main.humidity;
+                const maxTemp = data.main.temp_max;
+                const minTemp = data.main.temp_min;
+                const country = data.sys.country;
+                const desc = (data.weather[0].description).toUpperCase();
+                locationA.textContent = `${location}, ${country}`;
+                temperatureDegree.textContent = temp.toFixed(2);
+                dayMaxMin.textContent = `MAX: ${maxTemp} ºC MIN: ${minTemp} ºC`;
+                tempHumidity.textContent = `HUMIDITY: ${humid}%`;
+                console.log(data.weather);
+                temperatureDescription.textContent = `${desc}`;
+
+                document.getElementById('myIcon').className = `wi wi-owm-${iconNum}`;
+
+
+          
+
+                
+
+            });
+        });
+    }
+
+}); 
+
+    
+
+
